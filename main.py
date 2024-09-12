130340866ae8f633a9a9462831ee1c9abc00c7b4
Charan <charanv@devtools.in>
2024-09-12 15:32:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from jira_functions import get_projects
 
 def main():
     parser = argparse.ArgumentParser(description="Jira Projects CLI")
-    parser.add_argument('-gp', '--get-projects', action='store_true', help='Get Jira projects')
+    parser.add_argument('-gpa', '--get_all_projects', action='store_true', help='Get Jira projects')
     args = parser.parse_args()
 
     if not any(vars(args).values()):
         parser.print_help()
-    elif args.get_projects:
+    elif args.get_all_projects:
         get_projects()
 
 if __name__ == "__main__":
