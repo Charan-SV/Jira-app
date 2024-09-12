55ac1d06528ff9757d8d74ea5da81869bbf9044c
Charan <charanv@devtools.in>
2024-09-12 14:47:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# main.py
+import argparse
+from jira_functions import get_projects
+
+def main():
+    parser = argparse.ArgumentParser(description="Jira Projects CLI")
+    parser.add_argument('-gp', '--get-projects', action='store_true', help='Get Jira projects')
+    args = parser.parse_args()
+
+    if not any(vars(args).values()):
+        parser.print_help()
+    elif args.get_projects:
+        get_projects()
+
+if __name__ == "__main__":
+    main()
