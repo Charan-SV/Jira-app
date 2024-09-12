0737c7f45601021bfe73488590d2c67345435129
Charan <charanv@devtools.in>
2024-09-12 14:49:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name='jira',
     version='0.1',
     packages=find_packages(),
-    py_modules=['get_projects'],
+    py_modules=['main', 'jira_functions'],
     entry_points={
         'console_scripts': [
-            'jira=get_projects:main',
+            'jira=main:main',
         ],
     },
     install_requires=[
